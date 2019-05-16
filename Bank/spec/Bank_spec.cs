837a59599627b609b57d1592f9928a14948d4af5
alakijaayo <alakijaayo@gmail.com>
2019-05-16 14:07:28 +0100

    Adding Files and Initial Test
    Adding the Spec and src files that i will be using for testing and writing my code. Have also added my first test which currently fails.

---
+++ Bank/spec/Bank_spec.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+using src;
+
+namespace spec
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Balance()
+        {
+            var testbalance = new Bank();
+            var zero = testbalance.balance();
+            Assert.AreEqual(0, zero);
+        }
+    }
+}
